50c82e38b76cce28a6615d9d2f814286fdad8319
Prawee Wongsa <prawee@hotmail.com>
2025-07-21 03:28:46 +0700

    configure better-auth with open api

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,6 @@
 import { betterAuth } from 'better-auth'
 import { drizzleAdapter } from 'better-auth/adapters/drizzle'
+import { openAPI } from 'better-auth/plugins'
 import { db } from '@/database/config'
 
 export const auth = betterAuth({
@@ -8,5 +9,8 @@ export const auth = betterAuth({
     }),
     emailAndPassword: {
         enabled: true,
-    }
+    },
+    plugins: [
+        openAPI(),
+    ]
 })
